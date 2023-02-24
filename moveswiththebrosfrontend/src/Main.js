31a0683292c78125ae0c76dc76154d29cb2f1336
Andrew Ozoria <andrewozo123@gmail.com>
2023-02-24 16:38:33 -0500

    progress on activity component

--- moveswiththebrosfrontend/src/Main.js
+++ moveswiththebrosfrontend/src/Main.js
@@ -1,10 +1,36 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Activities from "./components/Activites/Activities";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Main() {
   return (
-    <div className="App">
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Meadows Moves
+            </Typography>
+            <Button color="inherit">Add New</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Routes>
         <Route path="/" element={<Activities />} />
       </Routes>
--- moveswiththebrosfrontend/src/components/Activites/Activities.js
+++ moveswiththebrosfrontend/src/components/Activites/Activities.js
@@ -1,8 +1,20 @@
 import React, { useEffect } from "react";
 import { fetchAllActivities } from "./activitiesSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
 
 function Activities() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   const dispatch = useDispatch();
 
   const activities = useSelector((state) => state.activities.allActivities);
@@ -14,7 +26,22 @@ function Activities() {
   console.log(activities);
   return (
     <div>
-      <p>Hello</p>
+      <div>
+        {activities.map((activity) => (
+          <div>
+            <Box sx={{ width: "100%" }}>
+              <Stack spacing={2}>
+                <Item>
+                  <p>{activity.spotName}</p>
+                  <p>{activity.amount}</p>
+                  <p>{activity.putCardDown}</p>
+                  <p>{activity.date}</p>
+                </Item>
+              </Stack>
+            </Box>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
